7c5175f639b263b3588211e29149888e060c36ca
Shashi <shashihghebad3@gmail.com>
2021-03-07 01:42:23 -0800

    [SHASHI]Add:UC2-PlayerRolls

---
+++ SNAKEANDLADDER/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public const int START_POINT = 0;
+        public const int END_POINT = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome To Snake And Lader Problem \nEnter player name");
+            string player1 = Console.ReadLine();
+            int diceRoll = DiceRoll();
+            Console.WriteLine("Dice Roll : " + diceRoll);
+        }
+        static int DiceRoll()
+        {
+            Random random = new Random();
+            int diceNum = random.Next(1, 7);
+            return diceNum;
+        }
+    }
+}
